ac3c03578e03903f30b7e72ec978f4e0f8e54fb9
burragokulkumar-boop <burragokulkumar@gmail.com>
2026-09-11 09:12:01 +0530

    Update KioskDeviceAdminReceiver.java

--- KioskDeviceAdminReceiver.java
+++ KioskDeviceAdminReceiver.java
@@ -1,7 +1,33 @@
 package com.infinitymeta.kiosk;
 
 import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
 
-public class KioskDeviceAdminReceiver
-        extends DeviceAdminReceiver {
-        }
+public class KioskDeviceAdminReceiver extends DeviceAdminReceiver {
+
+    @Override
+    public void onEnabled(
+            Context context,
+            Intent intent
+    ) {
+        Toast.makeText(
+                context,
+                "Infinity Meta Kiosk administrator enabled",
+                Toast.LENGTH_SHORT
+        ).show();
+    }
+
+    @Override
+    public void onDisabled(
+            Context context,
+            Intent intent
+    ) {
+        Toast.makeText(
+                context,
+                "Infinity Meta Kiosk administrator disabled",
+                Toast.LENGTH_SHORT
+        ).show();
+    }
+}
